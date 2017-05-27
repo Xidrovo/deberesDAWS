a0c375dcc502017c4f7449ded1fcf17abe8f7c6a
Xidrovo <xidrovo@hotmail.com>
2017-05-27 01:13:23 -0500

    agregado show more

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $( document ).ready(function(){
 	$('ul.tabs').tabs();
 })
+
+$(".show-more a").on("click", function() {
+  var $this = $(this);
+  var $content = $this.parent().prev("div.content");
+  var linkText = $this.text().toUpperCase();
+
+  if (linkText === "VER MÁS...") {
+    linkText = "Ver menos...";
+    $content.removeClass("hideContent");
+    $content.addClass("showContent");
+  } else {
+    linkText = "Ver más...";
+    $content.removeClass("showContent");
+    $content.addClass("hideContent");
+  };
+
+  $this.text(linkText);
+});
